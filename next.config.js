d08046c8017d3a8a25767d75dea538a0f92d95a2
jakkapant32 <jakkapant32@gmail.com>
2025-11-05 21:33:06 +0700

    Fix: Add path alias resolution in webpack config for Render.com

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias.canvas = false
+    config.resolve.alias['@'] = path.resolve(__dirname)
     return config
   },
 }
